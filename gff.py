17a9ec8f750e6dcddbfac9e7a4313212d39439cd
Alex Buchanan <buchanae@gmail.com>
2011-12-30 17:48:11 -0800

    Make gff.Reader a reusable iterator
    Also, get rid of csv.DictReader, allowing a parse() function for parsing a single line.

--- gff.py
+++ gff.py
@@ -1,12 +1,9 @@
-from csv import DictReader
-
-
 fieldnames = ('seqid', 'source', 'type', 'start', 'end', 
               'score', 'strand', 'phase', 'attributes')
 
 
 def parse_attributes(raw):
-    """Parse a GFF3 attribute string."""
+    """Parse a GFF3 attributes string."""
 
     att = {}
     for token in raw.split(';'):
@@ -15,20 +12,36 @@ def parse_attributes(raw):
             att[key] = value
     return att
 
-def reader(path):
+def parse(raw):
+    """Parse a GFF3 line."""
+
+    d = {}
+
+    for i, val in enumerate(raw.strip().split('\t')):
+        d[fieldnames[i]] = val
+
+    for k, v in parse_attributes(d['attributes']).iteritems():
+        d[k] = v
+
+    d['start'] = int(d['start'])
+    d['end'] = int(d['end'])
+
+    return d
+
+
+class Reader(object):
+
     """Read a GFF3 file, returning a dictionary for each line."""
 
-    def _reader(path):
-        with open(path) as fh:
-            for line in fh:
-                if line[:2] != '##':
-                    yield line
+    def __init__(self, path):
+        self.path = path
 
-    for row in DictReader(_reader(path), fieldnames, delimiter='\t'):
-        for key, value in parse_attributes(row['attributes']).iteritems():
-            row[key] = value
 
-        row['start'] = int(row['start'])
-        row['end'] = int(row['end'])
-        
-        yield row
+    def __iter__(self):
+        def _reader():
+            with open(self.path) as fh:
+                for line in fh:
+                    if line[:2] != '##':
+                        yield parse(line)
+
+        return _reader()
--- tests/test_gff.py
+++ tests/test_gff.py
@@ -10,31 +10,35 @@ def test_parse_attributes():
 
 def test_reader():
     p = files.path('gff')
-    lines = list(gff.reader(p))
-    eq_({
-        'seqid': 'Chr1',
-        'source': 'TAIR10',
-        'type': 'chromosome',
-        'start': 1,
-        'end': 30427671,
-        'score': '.',
-        'strand': '.',
-        'phase': '.',
-        'attributes': 'ID=Chr1;Name=Chr1',
-        'ID': 'Chr1',
-        'Name': 'Chr1',
-    }, lines[0])
-    eq_({
-        'seqid': 'Chr1',
-        'source': 'TAIR10',
-        'type': 'gene',
-        'start': 3631,
-        'end': 5899,
-        'score': '.',
-        'strand': '+',
-        'phase': '.',
-        'attributes': 'ID=AT1G01010;Note=protein_coding_gene;Name=AT1G01010',
-        'ID': 'AT1G01010',
-        'Note': 'protein_coding_gene',
-        'Name': 'AT1G01010',
-    }, lines[1])
+    r = gff.Reader(p)
+
+    # test that the Reader iterator can be reused
+    for i in xrange(2):
+        lines = list(r)
+        eq_({
+            'seqid': 'Chr1',
+            'source': 'TAIR10',
+            'type': 'chromosome',
+            'start': 1,
+            'end': 30427671,
+            'score': '.',
+            'strand': '.',
+            'phase': '.',
+            'attributes': 'ID=Chr1;Name=Chr1',
+            'ID': 'Chr1',
+            'Name': 'Chr1',
+        }, lines[0])
+        eq_({
+            'seqid': 'Chr1',
+            'source': 'TAIR10',
+            'type': 'gene',
+            'start': 3631,
+            'end': 5899,
+            'score': '.',
+            'strand': '+',
+            'phase': '.',
+            'attributes': 'ID=AT1G01010;Note=protein_coding_gene;Name=AT1G01010',
+            'ID': 'AT1G01010',
+            'Note': 'protein_coding_gene',
+            'Name': 'AT1G01010',
+        }, lines[1])
